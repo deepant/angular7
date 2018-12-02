36f8848176c084135a81e473986e0dc766c9b27b
souravlaha2009 <rahul@coursecube.com>
2018-12-02 09:50:18 +0530

    added header , footer, body components with login form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { BodyContainerComponent } from './body-container/body-container.component';
+import { FooterComponent } from './footer/footer.component';
+import { LoginFormComponent } from './body-container/login-form/login-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    BodyContainerComponent,
+    FooterComponent,
+    LoginFormComponent
   ],
   imports: [
     BrowserModule,
